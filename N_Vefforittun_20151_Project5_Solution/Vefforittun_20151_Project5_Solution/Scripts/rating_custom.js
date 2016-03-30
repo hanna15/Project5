388dd13c1ace164ab051f5cd4a520e5894eefe1c
hanna15 <hanna15@ru.is>
2016-03-30 15:13:35 +0000

    first commit

---
+++ N_Vefforittun_20151_Project5_Solution/Vefforittun_20151_Project5_Solution/Scripts/rating_custom.js
@@ -0,0 +1,52 @@
+﻿var logID = 'log',
+log = $('<div id="' + logID + '"></div>');
+$('body').append(log);
+$('[type*="radio"]').change(function() {
+    var me = $(this);
+    log.html(me.attr('value'));
+});
+
+$(function () {
+    $('form').on('submit', function () {
+        var form = $(this);
+        $.ajax()({
+            url: form.attr('action'),
+            data: form.serialize(),
+            mehtod: 'POST',
+            success: function (responseData) {
+                $('#rating').replaceWith($(respondData).find('#reviews'));
+            }
+        });
+        return false;
+    });
+});
+
+/*
+function stars() {
+    var rating = parseInt($("#myCurrentRating").text());
+
+    if (rating != 0) {
+        $(".rateLabel").addClass("text-warning");
+    }
+
+    for (var i = 1; i < 11; i++) {
+        var str = "rating_" + i;
+        if (i > rating) {
+            $('label[for="' + str + '"]').html("☆");
+        }
+        else {
+            $('label[for="' + str + '"]').html("★");
+        }
+    }
+}
+
+$(document).ready(function () {
+        $.post(theURL, rateForm, function (data) {
+            $("#averageRatingMsg").html('I rated <span id="myRating"></span>');
+            $("#myRating").text(data.CurrentUserRating.rating);
+            $("#myCurrentRating").text(data.CurrentUserRating.rating);
+            $("#ratingAverage").text(data.RatingOverall);
+            stars();
+        });
+    });
+});
---
+++ N_Vefforittun_20151_Project5_Solution/Vefforittun_20151_Project5_Solution/Scripts/review_custom.js
@@ -0,0 +1,16 @@
+﻿
+$(function () {
+    $('form').on('submit', function () {
+        var form = $(this);
+        $.ajax()({
+            url: form.attr('action'),
+            data: form.serialize(),
+            mehtod: 'POST',
+            success: function (responseData) {
+                $('#reviews').replaceWith($(respondData).find('#reviews'));
+            }
+        });
+        return false;
+    });
+});
+
